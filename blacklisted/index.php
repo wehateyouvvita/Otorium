9437f1ace10058e39aa1b05c34f89dfa92478295
vvita <98233732+wehateyouvvita@users.noreply.github.com>
2023-08-03 13:04:47 +0100

    Add files via upload

---
+++ blacklisted/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre.min.css">
+		<link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre-exp.min.css">
+		<link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre-icons.min.css">
+		<style>
+		body {
+			color: white !important;
+		}
+		</style>
+	</head>
+	<body style="background-color:rgb(50,50,50);">
+		<header class="navbar" style="background-color: rgb(50,50,50); border-bottom:1px solid rgb(20,20,20); padding:10px;">
+		  <section class="navbar-section">
+			<img src="https://cdn.otorium.xyz/assets/images/logo.png" height="26" alt="Logo">
+		  </section>
+		</header>
+		<br />
+		<br />
+		<div style="padding-left:7.5%;padding-right:7.5%;">
+		<h3>Hey.</h3>
+		<p>Depending on what you've done, you've been blacklisted. Sad....</p>
+		<p>How's life?</p>
+		</div>
+	</body>
+</html>
