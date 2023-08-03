25e3e0fd3a3fca58621314c80a978df1778ae265
vvita <98233732+wehateyouvvita@users.noreply.github.com>
2023-08-03 13:05:43 +0100

    Add files via upload

---
+++ currency/index.php
@@ -0,0 +1,20 @@
+<?php include $_SERVER['DOCUMENT_ROOT'].'/api/cfg/header.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php getHeader(); ?>
+</head>
+<body>
+
+<div class="sides">
+
+<div id="messages" style="border:1px solid rgb(200,200,200); padding:25px;">
+	<h4>Currency is not available at the moment</h4>
+</div>
+
+</div>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/api/cfg/footer.php'; ?>
+
+</body>
+</html>
